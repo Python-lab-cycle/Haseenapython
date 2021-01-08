b77e3e557bccbe8c82f589df77163a468f417218
HASEENA P A <76676457+haseenabadusha@users.noreply.github.com>
2021-01-08 15:05:18 +0530

    Add files via upload

---
+++ circlearea.py
@@ -0,0 +1,5 @@
+#area of circle
+import math
+r=float(input("ener the radious"))
+area=math.pi*r**2
+print("area=",area)
---
+++ largest.py
@@ -0,0 +1,19 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/largestof3.py =
+enter 1st number10
+enter 2nd number20
+enter 3rd number2
+Biggest of 10 20 2 is 20
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/largestof3.py =
+enter 1st number50
+enter 2nd number60
+enter 3rd number80
+Biggest of 50 60 80 is 80
+>>> 
